76e0e8ed077877a35672b712267764928e94e77e
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2022-01-31 11:36:43 +0100

    read part is done

--- assets/function/create.php
+++ assets/function/create.php
@@ -2,14 +2,14 @@
 include './db.php'; 
     
   // taking values from the form
-  $Name =  $_POST['name'];
+  $fName =  $_POST['name'];
   $Email = $_POST['Email'];
   $Phone =  $_POST['phone'];
   $Enrollnumber = $_POST['enroolnum'];
   $dateofadmin = $_POST['dateofadmission'];
     
   // adding it to the data base
-  $sql = "INSERT INTO persons VALUES (null,'$Name', 
+  $sql = "INSERT INTO students VALUES (null,'$fName', 
       '$Email','$Phone','$Enrollnumber','$dateofadmin')";
     
   if(mysqli_query($conn, $sql)){
