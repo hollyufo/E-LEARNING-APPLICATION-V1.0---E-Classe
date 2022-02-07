5fb1f8bc7287ae400730622b4eb1fa8b521f5d33
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2022-02-07 16:52:06 +0100

    updated smtp

--- controllers/reset.php
+++ controllers/reset.php
@@ -32,8 +32,8 @@ if(isset($_POST["reset"])){
                     $mail->isSMTP();                                            //Send using SMTP
                     $mail->Host       = 'smtp.hostinger.com';                     //Set the SMTP server to send through
                     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                    $mail->Username   = 'eclass@imranechaibi.com';                     //SMTP username
-                    $mail->Password   = 'dataCRYPTO123';                               //SMTP password
+                    $mail->Username   = 'e-class@imranechaibi.com';                     //SMTP username
+                    $mail->Password   = 'dataCRYPTO123@';                               //SMTP password
                     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
                     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
