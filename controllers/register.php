8610d2caf61aa443b90575b35e38c055920fc54f
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2022-02-08 13:36:53 +0100

    Update register.php

--- controllers/register.php
+++ controllers/register.php
@@ -124,7 +124,7 @@
                             $mail->addCustomHeader('Sender ID', 'e-class@imranechaibi.com');
                             //Recipients
                             $mail->setFrom('e-class@imranechaibi.com', 'imranechaibi');
-                            $mail->addAddress($email, 'E-   class team');     //Add a recipient
+                            $mail->addAddress($email, 'E-class team');     //Add a recipient
                             //$mail->addAddress('ellen@example.com');               //Name is optional
                             //$mail->addReplyTo('info@example.com', 'Information');
                             //$mail->addCC('cc@example.com');
