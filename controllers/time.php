3b6abe987deea71bb92e2832e0d9af9a32c90799
imrane chaibi <54860557+hollyufo@users.noreply.github.com>
2022-02-16 08:40:51 +0100

    Update time.php

--- controllers/time.php
+++ controllers/time.php
@@ -1,5 +1,5 @@
 <?php 
-    if(time() - $_SESSION['timestamp'] > 30) { //subtract new timestamp from the old one
+    if(time() - $_SESSION['timestamp'] > 10) { //subtract new timestamp from the old one
         echo"<script>alert('15 Minutes over!');</script>";
         unset($_SESSION['id'], $_SESSION['firstname'], $_SESSION['lastname'], $_SESSION['email'], $_SESSION['mobilenumber'], $_SESSION['token'], $_SESSION['login'], $_SESSION['login']);
         header('Location:  index.php'); //redirect to index.php
